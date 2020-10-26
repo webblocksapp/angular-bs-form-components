c62c35c03790fe027484a76a189c22d59532f821
Mauricio Rivera <mauriciorivera@iMac-de-mac.local>
2020-10-25 20:40:43 -0500

    update: added more data input components to overview example

--- src/app/docs/data-groups/demos/demo-overview-1/dtos/example-dto-1.dto.ts
+++ src/app/docs/data-groups/demos/demo-overview-1/dtos/example-dto-1.dto.ts
@@ -1,4 +1,8 @@
-import { IsEmail, IsNotEmpty } from '@webblocksapp/class-validator';
+import {
+  ArrayMinSize,
+  IsEmail,
+  IsNotEmpty,
+} from '@webblocksapp/class-validator';
 
 export class ExampleDto1 {
   @IsNotEmpty()
@@ -7,4 +11,16 @@ export class ExampleDto1 {
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
+  birthDate: string;
+
+  @IsNotEmpty()
+  gender: number;
+
+  @IsNotEmpty()
+  birthCity: number;
+
+  @ArrayMinSize(1)
+  favoriteFoods: Array<number>;
 }
--- src/app/docs/data-groups/demos/demo-overview-1/running-code.component.ts
+++ src/app/docs/data-groups/demos/demo-overview-1/running-code.component.ts
@@ -8,10 +8,30 @@ import { ExampleDto1 } from './dtos/example-dto-1.dto';
   templateUrl: './running-code.component.html',
 })
 export class RunningCodeComponent {
-  public ExampleModel1 = new BaseModel(ExampleDto1);
   public errors: Array<ValidationError> = null;
   public validatedData: ExampleDto1 = null;
   public JSON = JSON;
+
+  public ExampleModel1 = new BaseModel(ExampleDto1);
+
+  public genders = [
+    { value: 1, viewValue: 'Male' },
+    { value: 2, viewValue: 'Female' },
+  ];
+
+  public cities = [
+    { value: 1, viewValue: 'New York' },
+    { value: 2, viewValue: 'City of Mexico' },
+    { value: 3, viewValue: 'Quito' },
+    { value: 4, viewValue: 'Bogota' },
+  ];
+
+  public favoriteFoods = [
+    { value: 1, viewValue: 'Pizza' },
+    { value: 2, viewValue: 'Hamburger' },
+    { value: 3, viewValue: 'Ice cream' },
+    { value: 4, viewValue: 'Chinese rice' },
+  ];
 
   onSubmit(event) {
     event.then((validationResult) => {
