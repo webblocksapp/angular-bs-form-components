23fb95a58521c76550d83726e09da0fa4536227a
Mauricio Rivera <mauricio.rivera@decameron.com>
2020-12-05 12:01:25 -0500

    update: working on select2 api docs.

--- src/app/docs/bootstrap-components/select2/index.component.ts
+++ src/app/docs/bootstrap-components/select2/index.component.ts
@@ -6,13 +6,11 @@ import { DocsBaseRouter } from '@shared/classes';
   template: `
     <app-docs-container [markers]="markers">
       <h5 class="mb-4">Bootstrap Select2</h5>
-      <app-tabs [routes]="routes"></app-tabs>
+      <app-tabs></app-tabs>
       <div class="pt-3">
         <router-outlet (activate)="onActivate($event)"></router-outlet>
       </div>
     </app-docs-container>
   `,
 })
-export class IndexComponent extends DocsBaseRouter {
-  public routes = [{ title: 'Overview', path: 'overview' }];
-}
+export class IndexComponent extends DocsBaseRouter {}
