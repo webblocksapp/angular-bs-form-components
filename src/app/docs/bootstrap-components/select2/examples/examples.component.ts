81fecd9fbee15c15640ce6a30a9b1427501aad36
Mauricio Rivera <mauriver21@gmail.com>
2021-03-13 12:58:17 -0500

    fix: fixed marker directive on select2 examples.

--- src/app/docs/bootstrap-components/select2/examples/examples.component.ts
+++ src/app/docs/bootstrap-components/select2/examples/examples.component.ts
@@ -4,10 +4,10 @@ import { DocsBase } from '@shared/classes';
 @Component({
   selector: 'app-examples',
   template: `
-    <h5 #marker>Select2 with different sizes</h5>
+    <h5 marker>Select2 with different sizes</h5>
     <example-1></example-1>
 
-    <h5 #marker>Multiple selection</h5>
+    <h5 marker>Multiple selection</h5>
     <example-2></example-2>
   `,
 })
