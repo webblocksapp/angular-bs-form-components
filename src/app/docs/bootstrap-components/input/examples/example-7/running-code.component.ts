bd32a4b1f184a5884d04787946fa188819b6463f
Mauricio Rivera <mauriver21@gmail.com>
2021-03-24 08:27:39 -0500

    update: added validation result on input's example 7.

--- src/app/docs/bootstrap-components/input/examples/example-7/running-code.component.ts
+++ src/app/docs/bootstrap-components/input/examples/example-7/running-code.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BaseModel } from '@webblocksapp/ng-forms';
+import { ValidationResult } from 'projects/ng-forms/src/lib/data-group/types';
 import { DetailDto } from './detail.dto';
 import { GuestDto } from './guest.dto';
 import { HostDto } from './host.dto';
@@ -9,10 +10,18 @@ import { HostDto } from './host.dto';
   templateUrl: './running-code.component.html',
 })
 export class RunningCodeComponent {
+  public JSON = JSON;
+  public validationResult: ValidationResult;
   public userModel: BaseModel = new BaseModel(HostDto, {
     nested: [
       { path: 'guest', dtoClass: GuestDto },
       { path: 'guest.detail', dtoClass: DetailDto },
     ],
   });
+
+  onSubmit(event): void {
+    event.then((validationResult) => {
+      this.validationResult = validationResult;
+    });
+  }
 }
