a1317304fe518d33f7f1fe9bc1b35942cef908e6
Mauricio Rivera <mauriver21@gmail.com>
2021-04-13 10:14:19 -0500

    update: implemented floating markers.

--- src/app/shared/components/overview/overview.component.ts
+++ src/app/shared/components/overview/overview.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { MarkerDirective } from '@shared/directives/marker.directive';
 
 @Component({
   selector: 'app-overview',
   template: `
-    <div class="pl-3" *ngIf="markers.length">
+    <div #markersWrapper class="pl-3 position-absolute" *ngIf="markers.length">
       <ul class="navbar-nav">
         <li
           class="nav-item dropdown"
@@ -38,11 +44,41 @@ import { MarkerDirective } from '@shared/directives/marker.directive';
     `,
   ],
 })
-export class OverviewComponent {
+export class OverviewComponent implements AfterViewInit {
+  @ViewChild('markersWrapper', { read: ElementRef }) markersWrapper: ElementRef;
+
   @Input() markers: Array<MarkerDirective>;
+
+  ngAfterViewInit(): void {
+    this.updatePosition();
+
+    window.addEventListener('scroll', () => {
+      this.updatePosition();
+    });
+
+    window.addEventListener('resize', () => {
+      this.updatePosition();
+    });
+  }
 
   scrollToMarker(element): void {
     const offsetTop = element.offsetTop;
     window.scrollTo(0, offsetTop);
+  }
+
+  updatePosition(): void {
+    const markersWrapper = this.markersWrapper.nativeElement;
+    const parentElement = markersWrapper.parentElement;
+
+    let scrollY = window.scrollY;
+    let parentElementPositionY =
+      window.scrollY + parentElement.getBoundingClientRect().top;
+
+    if (scrollY > parentElementPositionY) {
+      markersWrapper.style.top = scrollY - parentElementPositionY + 20 + 'px';
+      return;
+    }
+
+    markersWrapper.style.top = 0;
   }
 }
