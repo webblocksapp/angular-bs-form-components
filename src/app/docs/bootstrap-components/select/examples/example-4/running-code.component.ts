a291693f15bbc68d219a1d1272a16f13c660ba45
Mauricio Rivera <mauriver21@gmail.com>
2021-06-01 15:15:02 -0500

    update: added bs-select events example.

---
+++ src/app/docs/bootstrap-components/select/examples/example-4/running-code.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { BaseModel } from '@webblocksapp/ng-forms';
+import { SelectOption } from '@webblocksapp/ng-forms';
+import { ExampleDto } from './example.dto';
+
+@Component({
+  selector: 'running-code',
+  templateUrl: './running-code.component.html',
+})
+export class RunningCodeComponent {
+  public exampleModel: BaseModel = new BaseModel(ExampleDto);
+
+  public genders: SelectOption[] = [
+    { value: 1, viewValue: 'Male' },
+    { value: 2, viewValue: 'Female' },
+    { value: 3, viewValue: 'Other' },
+  ];
+
+  public eventHTML: string = 'No event emitted';
+
+  onShownEvent(event): void {
+    this.eventHTML = `<b>shownEvent</b> emitted with value ${event.target.value}`;
+  }
+
+  onHiddenEvent(event): void {
+    this.eventHTML = `<b>hiddenEvent</b> emitted with value ${event.target.value}`;
+  }
+}
