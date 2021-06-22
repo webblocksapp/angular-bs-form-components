20cdafc691e24171f48ef2031ea58e54e8e0ad05
Mauricio Rivera <mauriver21@gmail.com>
2021-06-22 08:57:57 -0500

    update: created vanilla multi select component.

--- src/app/shared/components/vanilla-components/vanilla-components.module.ts
+++ src/app/shared/components/vanilla-components/vanilla-components.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextInputComponent } from './text-input/text-input.component';
 import { SelectInputComponent } from './select-input/select-input.component';
+import { MultiSelectInputComponent } from './multi-select-input/multi-select-input.component';
 
 @NgModule({
-  declarations: [TextInputComponent, SelectInputComponent],
+  declarations: [
+    TextInputComponent,
+    SelectInputComponent,
+    MultiSelectInputComponent,
+  ],
   imports: [CommonModule],
-  exports: [TextInputComponent, SelectInputComponent],
+  exports: [
+    TextInputComponent,
+    SelectInputComponent,
+    MultiSelectInputComponent,
+  ],
 })
 export class VanillaComponentsModule {}
